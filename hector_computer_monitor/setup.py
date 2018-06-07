91729c366ca4373f7935128480bf4a9ba6709a8e
Johannes Meyer <johannes@intermodalics.eu>
2018-06-08 01:27:03 +0200

    hector_computer_monitor: fix non-existent package_dir in setup.py, use catkin_install_python() macro
    Because the package does not contain a Python module, having a setup.py and calling `catkin_python_setup()`
    would not even be necessary.
    
    See http://wiki.ros.org/catkin/CMakeLists.txt#Installing_Python_Executable_Scripts.

--- hector_computer_monitor/setup.py
+++ hector_computer_monitor/setup.py
@@ -4,8 +4,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['hector_computer_monitor'],
-    package_dir={'': 'srcripts'})
+setup_args = generate_distutils_setup()
 
 setup(**setup_args)
