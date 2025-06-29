481a3c59feb8a500d0018405bebec860d5a2db00
Jonathan <jonathan.lichtenfeld@gmail.com>
2025-06-29 12:21:58 +0200

    Cleanup

--- hector_diagnostic_aggregator/src/hardware_analyzer.cpp
+++ hector_diagnostic_aggregator/src/hardware_analyzer.cpp
@@ -1,3 +1,43 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2009, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/**< \author Kevin Watts */
+/**< \author Arne Nordmann */
+
+// Changelog Jonathan Lichtenfeld 29.06.2025
+// - Refactored to match by hardware_id.
+
 #include <hector_diagnostic_aggregator/hardware_analyzer.hpp>
 
 #include <map>
@@ -44,12 +84,14 @@ bool HardwareAnalyzer::init(
     string pname = param.first;
     rclcpp::Parameter pvalue = param.second;
 
+    // Changes start here
     if (pname.compare("hardware_id") == 0) {
       RCLCPP_DEBUG(
         rclcpp::get_logger("HardwareAnalyzer"), "HardwareAnalyzer '%s' found hardware_id: %s",
         nice_name_.c_str(), pvalue.value_to_string().c_str());
       hardware_id_ = pvalue.as_string();
     }
+    // Changes end here
 
     if (pname.compare("path") == 0) {
       RCLCPP_DEBUG(
@@ -160,11 +202,6 @@ bool HardwareAnalyzer::match(const string & name)
     name.c_str());
 
   std::cmatch what;
-  // for (unsigned int i = 0; i < hardware_id_.size(); ++i) {
-  //   if (name == hardware_id_[i]) {
-  //     RCLCPP_INFO(  
-
-
   for (unsigned int i = 0; i < regex_.size(); ++i) {
     if (std::regex_match(name.c_str(), what, regex_[i])) {
       RCLCPP_INFO(
